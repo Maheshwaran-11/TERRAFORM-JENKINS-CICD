b4e22cbc6afe8498237fd042b34f1506280b9e06
Maheshwaran-11 <81170816+Maheshwaran-11@users.noreply.github.com>
2025-03-26 16:40:47 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
-  provider = aws.useast1
+  provider = aws
   bucket = var.bucket_name
 }
 resource "aws_s3_bucket_ownership_controls" "example" {
