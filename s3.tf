1751b2ee2bb3d71bb40a05e8f8a271541570c18f
Maheshwaran-11 <maheshwarank11@gmail.com>
2025-03-27 11:52:38 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,55 +1,65 @@
-# Create S3 Bucket
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
 }
 
-# Enforce Ownership Controls
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-# Block Public Access
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-# Upload Objects to S3 without ACLs
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+# Upload static files
 resource "aws_s3_object" "index" {
-  bucket        = aws_s3_bucket.mybucket.id
-  key           = "index.html"
-  source        = "index.html"
-  content_type  = "text/html"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket        = aws_s3_bucket.mybucket.id
-  key           = "error.html"
-  source        = "error.html"
-  content_type  = "text/html"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "style" {
-  bucket        = aws_s3_bucket.mybucket.id
-  key           = "style.css"
-  source        = "style.css"
-  content_type  = "text/css"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "style.css"
+  source = "style.css"
+  acl = "public-read"
+  content_type = "text/css"
 }
 
 resource "aws_s3_object" "script" {
-  bucket        = aws_s3_bucket.mybucket.id
-  key           = "script.js"
-  source        = "script.js"
-  content_type  = "text/javascript"
+  bucket = aws_s3_bucket.mybucket.id
+  key = "script.js"
+  source = "script.js"
+  acl = "public-read"
+  content_type = "text/javascript"
 }
 
-# Configure S3 Website
+# Configure S3 bucket as a website
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
   index_document {
@@ -58,4 +68,5 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = "error.html"
   }
+  depends_on = [aws_s3_bucket_acl.example]
 }
